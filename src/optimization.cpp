34474f74f73c51331fc8e2891a91115905491f8d
Denis <denisk@wustl.edu>
2023-12-22 22:53:00 -0600

    typo

--- src/optimization.cpp
+++ src/optimization.cpp
@@ -149,7 +149,8 @@ Rcpp::List derivative_stage2(const arma::mat& X,
     arma::mat new_Omega = Omega;
     arma::mat new_D_w = D_w;
     arma::mat new_D_h = new_D_w * (N / M);
-    arma::mat sqrt_N(cell_types, 1).fill(1/sqrt(N));
+    arma::mat sqrt_N(cell_types, 1);
+    sqrt_N.fill(1/sqrt(N));
 
     arma::vec Sigma = arma::diagvec(SVRt);
     arma::vec sqrt_Sigma = arma::sqrt(Sigma);
