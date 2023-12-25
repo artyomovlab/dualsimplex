b8041eb3e16392ca43bf8e4779a8351e086ce5ad
Denis <denisk@wustl.edu>
2023-12-25 10:59:37 -0600

    prints

--- src/optimization.cpp
+++ src/optimization.cpp
@@ -179,7 +179,9 @@ Rcpp::List derivative_stage2(const arma::mat& X,
           //  -2 * (diagmat(new_D_w) * new_Omega.t() * (SVRt - new_Omega * diagmat(new_D_w) * new_X));
         //der_X += coef_hinge_H * hinge_der_proportions_C__(new_X * R, R);
         //der_X += coef_pos_D_h * 2 * new_D_h * (new_X.t() * new_D_h - sum_rows_R).t();
+        Rcout << "going to manual update der_x" << std::end
         der_X.col(0) = der_X.at(0,0);
+
 
         // Update X
         new_X = new_X - coef_der_X * der_X;
@@ -192,6 +194,7 @@ Rcpp::List derivative_stage2(const arma::mat& X,
       //      -2 * (SVRt - new_Omega * diagmat(new_D_w) * new_X) * new_X.t() * diagmat(new_D_w);
 //        der_Omega += coef_hinge_W * hinge_der_basis_C__(S.t() * new_Omega, S);
 //        der_Omega += coef_pos_D_w * 2 * (new_Omega * new_D_w - sum_rows_S) * new_D_w.t();
+        Rcout << "going to manual update der_Omega" << std::end
         der_Omega.row(0) = der_Omega.at(0, 0);
 //        der_Omega = correctByNorm(der_Omega) * mean_radius_Omega;
 
