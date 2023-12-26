893b518e0e7f2f44aa4f8fc6f16b769e7bcc53ff
Denis <denisk@wustl.edu>
2023-12-26 13:09:54 -0600

    D as mean

--- src/optimization.cpp
+++ src/optimization.cpp
@@ -245,11 +245,7 @@ Rcpp::List derivative_stage2(const arma::mat& X,
         Rcpp::Rcout <<  new_D_w_omega << std::endl;
         new_D_w = (new_D_w_x + new_D_w_omega)/2;
         Rcpp::Rcout << "Took mean of these values as " << std::endl;
-        Rcpp::Rcout << new_D_w << std::
-
-
-
-
+        Rcpp::Rcout << new_D_w << std::endl;
         //Rcpp::Rcout << "delete by sqrt N" << std::endl;
       //  Rcpp::Rcout << "Square of this" << std::endl;
        // Rcpp::Rcout << "sqrt D_w from X is " << new_D_w << std::endl;
