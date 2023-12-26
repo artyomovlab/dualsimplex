22e120d163519af105f544ec27378ea2da13de9f
Denis <denisk@wustl.edu>
2023-12-26 13:56:10 -0600

    respective D_w for X and omega

--- src/optimization.cpp
+++ src/optimization.cpp
@@ -267,8 +267,8 @@ Rcpp::List derivative_stage2(const arma::mat& X,
     }
 
 
-    return Rcpp::List::create(Rcpp::Named("new_X") = arma::diagmat(1/new_D_w) * new_X,
-                              Rcpp::Named("new_Omega") = new_Omega * arma::diagmat(1/new_D_w),
+    return Rcpp::List::create(Rcpp::Named("new_X") = arma::diagmat(1/new_D_w_x) * new_X,
+                              Rcpp::Named("new_Omega") = new_Omega * arma::diagmat(1/new_D_w_omega),
                               Rcpp::Named("new_D_w") = new_D_w,
                               Rcpp::Named("new_D_h") = new_D_h,
                               Rcpp::Named("errors_statistics") = errors_statistics,
