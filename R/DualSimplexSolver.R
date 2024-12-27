1656f87bf0d18af52b497ff41f4fe2fe67a35c3c
Chia-Jung Lee <cjlee05016@gmail.com>
2024-12-27 11:30:06 -0600

    Fix $set_data bug

--- R/DualSimplexSolver.R
+++ R/DualSimplexSolver.R
@@ -171,7 +171,7 @@ DualSimplexSolver <- R6Class(
       if (!inherits(data, "ExpressionSet")) data <- create_eset(data)
       self$st$data <- add_default_anno(data, gene_anno_lists, sample_anno_lists)
       self$st$scaling <- sinkhorn_scale(exprs(self$st$data), max_iter = sinkhorn_iterations)
-      self$st$proj_ops <- calc_svd_ops(self$scaling$V_row, max_dim = max_dim, tol = tol)
+      self$st$proj_ops <- calc_svd_ops(self$st$scaling$V_row, max_dim = max_dim, tol = tol)
       self$st$proj <- svd_project(self$st$scaling, dims = NULL, ops = self$st$proj_ops)
       if (first_set) private$add_filtering_log_step("initial")
     },
