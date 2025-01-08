97d3e09a3d4f54c9ad2c18ad02d83a2972a424b7
Denis <denklewer@gmail.com>
2025-01-08 16:01:47 -0600

    build fixed

--- R/RcppExports.R
+++ R/RcppExports.R
@@ -236,3 +236,15 @@ efficient_sinkhorn <- function(V, max_iter = 20L, iter_start_check = 5L, check_e
     .Call('_DualSimplex_efficient_sinkhorn', PACKAGE = 'DualSimplex', V, max_iter, iter_start_check, check_every_iter, epsilon)
 }
 
+#' Helper function to perfrom iteration scaling
+#'
+#' @param V matrix to scale.
+#' @param D_vs_row matrix stroing row scaling at each iteration  
+#' @param D_vs_col matrix stroing column scaling at each iteration
+#' @param iter iteration at which Sinkhorn is converged
+#' @return scaled matrix
+#' @export
+sinkhorn_sweep_c <- function(V, D_vs_row, D_vs_col, iter) {
+    .Call('_DualSimplex_sinkhorn_sweep_c', PACKAGE = 'DualSimplex', V, D_vs_row, D_vs_col, iter)
+}
+
