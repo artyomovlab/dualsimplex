58a271c0f3d4f11e0c527eac5cbd281a751961bf
Denis <denklewer@gmail.com>
2025-01-13 18:57:25 -0600

    fix zero case again

--- src/sinkhorn.cpp
+++ src/sinkhorn.cpp
@@ -142,17 +142,16 @@ arma::mat sinkhorn_sweep_c(const arma::mat& V,
 
     arma::mat V_ = V;
     iter -= 1;  // adjust 1-base to 0-base
-    unsigned int i;
     // scaling till iter-1 round of normalization
-    for (i = 0; i < iter; i++) {
+    for (unsigned int i = 0; i < iter; i++) {
         V_.each_col() %= D_vs_row.col(i);
         V_.each_row() %= D_vs_col.col(i).t();
     }
 
     // Last normalization, returning V_row or V_column is controled by the size of D_vs_row and D_vs_col
-    V_.each_col() %= D_vs_row.col(i);
+    V_.each_col() %= D_vs_row.col((iter > 0) ? iter : 0);
     if (return_col_norm == 1) {
-        V_.each_row() %= D_vs_col.col(i).t();
+        V_.each_row() %= D_vs_col.col((iter > 0) ? iter : 0).t();
     }
 
     return(V_);
