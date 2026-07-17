6c2bafe36fd26d50cfc4db1958a10fce94a4d40a
Denis <denklewer@gmail.com>
2026-07-17 16:20:45 -0500

    generate random scale for X vectors

--- R/initialization.R
+++ R/initialization.R
@@ -193,7 +193,7 @@ initializers <- list(
     # We want to ensure that X_dtilda and Omega_dtilda are inverse
     # and have all positive first row/column respectively
 
-    # First we generate d candidates  generationg vector whos elements sum to M
+    # First we generate d candidates  generationg vector whose elements sum to M
     d_elements <-  runif(n_cell_types, min = 1e-5, max = 1.0)
     d_elements <- M * d_elements / sum(d_elements)
 
@@ -210,9 +210,10 @@ initializers <- list(
     # However orthonormality of N_r is not necessary for us
     # We break it with the random invertible matrix
     dim_null <- ncol(N_r)
-    W <- matrix(runif(dim_null * dim_null, min = -1.0, max = 1.0), nrow = dim_null)
+    random_scale <- 10^runif(1, min = -1.5, max = 1.5) # to have random relation in sizes between X and Omega
+    W <- matrix(runif(dim_null * dim_null, min = -random_scale, max = random_scale), nrow = dim_null)
     # Force it to be diagonally dominant so it is computationally stable (invertible)
-    diag(W) <- rowSums(W) + 1.0
+    diag(W) <- rowSums(W) + random_scale
     # Scramble the null space. V is now linearly independent, in the null space,
     # but NO LONGER orthonormal.
     V <- N_r %*% W
